2c94e433c257cace09b31aaddd3dc1ac93957a61
dacey.ennis <barrett.dacey.ennis@gmail.com>
2016-06-26 04:56:54 -0400

    refactor: Edited jokes.

--- jokes/src/main/java/com/example/android/ennis/barrett/Joker.java
+++ jokes/src/main/java/com/example/android/ennis/barrett/Joker.java
@@ -7,13 +7,13 @@ public class Joker {
             "Did you try using the duck?",
             "Compiling",
             "If its not broken we better get ready to update it",
-            "Best way to sort,\nwhile not isInOrder(desk)\n{shuffle(deck)}",
-            "Watch out for velociraptor\n after goto statements",
-            "There are many right solutions...\nMost of them are garbage",
-            "There is no cloud.\nJust someone else's computer",
+            "This is the best way to sort:\n\twhile not isInOrder(deck)\n\t{shuffle(deck)}",
+            "Watch out for velociraptors\n after goto statements",
+            "There are many right solutions in programming.\nMost of them are bad practices",
+            "There is no cloud...\nIts just someone else's computer )':",
             "This phone has a virus!",
-            "Programming is the simplest thing in the world...\nif you know what you're doing",
-            "while(hasCoffee==true)\n{programming=true}"
+            "Programming is simple...\nas long as you know what you're doing",
+            "while(hasCoffee==true) {\n\tprogramming=true;\n}"
     };
     private static final int Range = jokes.length - 1;
     private static final Random random = new Random();
